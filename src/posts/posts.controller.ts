afa984895bfebce17d52abc4d15cf21301a28031
정삼복 <68502584+3boku@users.noreply.github.com>
2024-03-28 00:22:32 +0900

    study

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,18 +18,99 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 124213124,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래연습 하고 있는 해린',
+    likeCount: 1000000,
+    commentCount: 124213124,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랭픽크 로제',
+    content: '종합운동장에서 공연하는 로제',
+    likeCount: 1000000,
+    commentCount: 124213124,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 10000900,
-      commentCount: 10000900,
+  getPosts() {
+    return posts;
+  } //모든 post를 가져온다.
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  } //파라미터에 해당되는 post만 가져오기.
+
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
     };
+
+    posts = [...posts, post];
+
+    return post;
+  }
+
+  @Patch(':id')
+  patchPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
   }
 }
