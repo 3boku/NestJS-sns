a0c37dd21bfc2bb939167f33daef88bca3fef9ab
정삼복 <68502584+3boku@users.noreply.github.com>
2024-04-22 20:25:01 +0900

    sql

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,49 +1,24 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-export interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 1000000,
-    commentCount: 124213124,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '노래연습 하고 있는 해린',
-    likeCount: 1000000,
-    commentCount: 124213124,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랭픽크 로제',
-    content: '종합운동장에서 공연하는 로제',
-    likeCount: 1000000,
-    commentCount: 124213124,
-  },
-];
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Repository } from 'typeorm';
+import { PostModel } from './entities/posts.entity';
 
 @Injectable()
 export class PostsService {
-  getAllPosts() {
-    return posts;
+  constructor(
+    @InjectRepository(PostModel)
+    private readonly postsRepository: Repository<PostModel>,
+  ) {}
+  async getAllPosts() {
+    return this.postsRepository.find();
   }
 
-  getPostById(id: number) {
-    const post = posts.find((post) => post.id === +id);
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
     if (!post) {
       throw new NotFoundException();
@@ -52,54 +27,54 @@ export class PostsService {
     return post;
   }
 
-  createPost(author: string, title: string, content: string) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
+  async createPost(author: string, title: string, content: string) {
+    const post = this.postsRepository.create({
       author,
       title,
       content,
       likeCount: 0,
       commentCount: 0,
-    };
+    });
 
-    posts = [...posts, post];
+    const newPost = await this.postsRepository.save(post);
 
-    return post;
+    return newPost;
   }
 
-  updatePost(postId: number, author: string, title: string, content: string) {
-    const post = posts.find((post) => post.id === postId);
+  async updatePost(
+    postId: number,
+    author: string,
+    title: string,
+    content: string,
+  ) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id: postId,
+      },
+    });
 
     if (!post) {
       throw new NotFoundException();
     }
-
     if (author) {
       post.author = author;
     }
-
     if (title) {
       post.title = title;
     }
-
     if (content) {
       post.content = content;
     }
-
     posts = posts.map((prevPost) => (prevPost.id === postId ? post : prevPost));
-
     return post;
   }
 
   deletePost(postId: number) {
-    const post = posts.find((post) => post.id === postId);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== postId);
-
-    return postId;
+    // const post = posts.find((post) => post.id === postId);
+    // if (!post) {
+    //   throw new NotFoundException();
+    // }
+    // posts = posts.filter((post) => post.id !== postId);
+    // return postId;
   }
 }
